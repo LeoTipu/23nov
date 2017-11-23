bb8e369d1d97d338535cc13f8c8c1f39c4d23bf7
User-44 <user-44@iot.datasoft.com>
2017-11-23 13:25:45 +0600

    first commit

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const app = express();
+
+app.get('/light', function(req,res){
+    res.send('light data')
+})
+
+app.get('/temp', function(req,res){
+    res.send('temp data')
+})
+
+app.get('/sound', function(req,res){
+    res.send('sound data')
+})
+
+app.listen(8081,function() {
+    console.log('app listening on 8081')
+    
+})
